a102f2e280c386fbd824bf01311ddc608d6db839
Ben Leppke <benleppke@gmail.com>
2025-09-09 19:17:26 -0500

    Simplify toggle js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,5 +1,8 @@
-document.querySelector('.nav-toggle').addEventListener('click', function() {
-    document.querySelector('.nav-right').classList.toggle('active');
+const navToggle = document.querySelector('.nav-toggle');
+const navRight = document.querySelector('.nav-right');
+
+navToggle.addEventListener('click', function() {
+    navRight.classList.toggle('active');
 });
 
 const themeToggle = document.querySelector('.theme-toggle');
@@ -40,15 +43,19 @@ themeToggle.addEventListener('click', () => {
 });
 
 function updateToggleIcons() {
+    const moonIcon = themeToggle.querySelector('.fa-moon');
+    const sunIcon = themeToggle.querySelector('.fa-sun');
+    const adjustIcon = themeToggle.querySelector('.fa-adjust');
     const icons = themeToggle.querySelectorAll('i');
+    
     icons.forEach(icon => icon.style.display = 'none');
     
     if (currentTheme === 'dark') {
-        themeToggle.querySelector('.fa-moon').style.display = 'inline-block';
+        moonIcon.style.display = 'inline-block';
     } else if (currentTheme === 'light') {
-        themeToggle.querySelector('.fa-sun').style.display = 'inline-block';
+        sunIcon.style.display = 'inline-block';
     } else {
-        themeToggle.querySelector('.fa-adjust').style.display = 'inline-block';
+        adjustIcon.style.display = 'inline-block';
     }
 }
 
